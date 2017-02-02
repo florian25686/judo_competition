7bc69d69b38238a2b5dee0354831148e5803fb1d
florian25686 <florianvo@gmail.com>
2017-02-02 11:54:11 +0100

    Implemented aUserlogin. Rights system and so on will be implemented later

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,12 @@ class DefaultController extends Controller
         return $this->render('Default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+    
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction() {
+        return new Response('<html><body>Admin Page!</body></html>');
     }
 }
