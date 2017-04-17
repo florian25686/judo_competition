cbb1a564112aca38b7d15eaec8d6353bb9ce8e89
florian25686 <florianvo@gmail.com>
2017-04-17 20:11:14 +0200

    Added Formfields template and AgeGroup edit

---
+++ src/AppBundle/Form/AgeGroupType.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AgeGroupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('save', SubmitType::class, array('label' => 'create.agegroup.button'))
+            ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\AgeGroups',
+        ));
+    }
+}
